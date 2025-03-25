d298ebf10b51b059c76e0bbc3125e300c9dc83f9
fchaverol <fchaverol@megacable.com.mx>
2025-03-24 18:16:04 -0600

    ferchaverol
    Fernando Chavero
    fchaverol@megacable.com.mx

---
+++ Modulo_1_Javascript_Testing/01_sesion/codigojava.js
@@ -0,0 +1,20 @@
+var numero = 12345;
+var texto = "texto";
+var booleano = true;
+var nombre = "Fernando";
+var apellido = "Chavero";
+var edad = 47;
+var nombrecompleto = `${nombre} ${apellido}`;
+
+console.log(`Hola, soy  ${nombrecompleto} y tengo ${edad} años`);
+console.log("Hola, soy " + nombrecompleto + " y tengo " + edad + " años");
+
+// Comparación de operadores
+// ==, ===, !=, !==, <, >, >=, <=;
+
+var numero1 = 5;
+var numerotexto = "5";
+console.log(numero1 === numerotexto); // false
+console.log(numero1 == numerotexto);  // true
+
+//Actividad
